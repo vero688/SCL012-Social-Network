f97b405d9580ff0a3f73bfd86f8fc9aed66683b2
SusejR <rodriguezbsusej@gmail.com>
2020-01-17 09:25:43 -0300

    agregando registro de usuario

--- src/main.js
+++ src/main.js
@@ -1,5 +1,38 @@
-// Este es el punto de entrada de tu aplicacion
+// // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+// import { myFunction } from './lib/index.js';
 
-myFunction();
+// myFunction();
+
+const btnRegistro= document.getElementById('enviar').addEventListener('click', registrar)
+const btnEntrar = document.getElementById('entrar').addEventListener('click', entrar)
+
+function registrar() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+};
+
+function entrar() {
+    const email2 = document.getElementById('email2').value;
+    const password2 = document.getElementById('password2').value;
+    
+    firebase.auth().signInWithEmailAndPassword(email2, password2)
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+}
