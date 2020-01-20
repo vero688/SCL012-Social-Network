6803bfc34fdb9e7bc78d1b54a92c375936c8353c
SusejR <rodriguezbsusej@gmail.com>
2020-01-20 09:32:38 -0300

    inicio htlm dinamico

--- src/main.js
+++ src/main.js
@@ -4,13 +4,20 @@
 
 // myFunction();
 
-const btnRegistro= document.getElementById('enviar').addEventListener('click', registrar)
+const btnRegistro= document.getElementById('registrarme').addEventListener('click', registrarme)
 const btnEntrar = document.getElementById('entrar').addEventListener('click', entrar)
+
+function registrarme() {
+  const root = document.getElementById('root')
+    root.innerHTML=  `<h4>Registro de Usuarios</h4>
+  <input id="email" type="email" placeholder="ingresa tu Email">
+  <input id="password" type="password" placeholder="Ingresa tu contraseña">`
+}
 
 function registrar() {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-
+    
     firebase.auth().createUserWithEmailAndPassword(email, password)
     .catch(function(error) {
         // Handle Errors here.
@@ -36,3 +43,22 @@ function entrar() {
         console.log(errorMessage);
       });
 }
+ function observador() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+    } else {
+      // User is signed out.
+      // ...
+    }
+  });
+ }
+ observador();
