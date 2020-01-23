2c263add5d346299b34c4d798031eb8c0887b674
Varinea <53873011+varinea@users.noreply.github.com>
2020-01-23 10:56:43 -0300

    Cerrar Sesión lista (#5)
    * enter
    
    * html
    
    * agregndo firebase del proyecto de master
    
    * probando colaborador con master
    
    * enter
    
    * gitignore
    
    * Probando 1eros cambios "Buscador principal en Muro"
    
    * Boton cerrar sesión funcionando tikitaka
    
    * textos para identificar código
    
    Co-authored-by: SusejR <57444604+SusejR@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ const btnRegister= document.getElementById('registerMe').addEventListener('click
 const btnLogIn = document.getElementById('logIn').addEventListener('click', logIn)
  
 
+// REGISTRAR USUARIO
 function registerMe() {
   const root = document.getElementById('root')
     root.innerHTML=  `<h4>Registro de Usuarios</h4>
@@ -27,14 +28,27 @@ function register() {
         // ...
         console.log(errorCode);
         console.log(errorMessage);
-      });
+      });    
 };
 
-function logIn() {
+// LOGEARSE Y CERRAR SESIÓN
+function logIn() { /* VARINEA meti mano*/
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    root.innerHTML = `<button id="btnSignOff">Cerrar Sesión</button>`
+    root.innerHTML = `
+
+    <h1>DP</h1>
+    <br>
+    <button id="homeMuro">HOME</button>
+    <button id="perfilUsuario">fotoUsuario</button>
+    <br>
+    <!-- Buscador -->
+    <input type="text" id="searchMuro" class="searchClass" placeholder="Buscador de DovePLayer"></input>
+
+    <!-- Cerrar sesión -->
+    <button id="btnSignOff">Cerrar Sesión</button>
+    `  
    
    document.getElementById('btnSignOff').addEventListener('click', signOff)
     firebase.auth().signInWithEmailAndPassword(email, password)
@@ -44,20 +58,25 @@ function logIn() {
         let errorMessage = error.message;
         // ...
         console.log(errorCode);
-        console.log(errorMessage);
+        console.log(errorMessage);s
       });
 
 }
+
+// funcion CERRAR SESIÓN
 function signOff() {
   
   firebase.auth().signOut()
   .then(function(){
-    console.log('saliendo....')
+    document.location.href="/";
+    //console.log('saliendo....')
   })
   .catch(function(error){
-    console.log(error)
+    console.log('error')
   });
 }
+
+
  function observer() {
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
