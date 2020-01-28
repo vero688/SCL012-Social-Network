78fadda6ab70f809f22952d5ca5a0fb904be0607
SusejR <rodriguezbsusej@gmail.com>
2020-01-28 09:11:17 -0300

    agregando alert por campo vacio

--- src/main.js
+++ src/main.js
@@ -3,10 +3,78 @@
 // import { myFunction } from './lib/index.js';
 
 // myFunction();
+showLogIn();
  document.getElementById('registerMe').addEventListener('click', registerMe)
-document.getElementById('logIn').addEventListener('click', logIn)
 
+//  Función que crea la pagina de inicio
+function showLogIn () {
+  root.innerHTML = ` <h4>Ingreso de Usuarios</h4>
+ <form> 
+  <input id="email" type="email" placeholder="ingresa tu Email" required>
+  <input id="password" type="password" placeholder="Ingresa tu contraseña" required>
+  <button id="logIn">Iniciar Sesión</button>
+  </form>
+  <img class="witch" src="img/brujo.png">
+  <button id="registerMe">Registrarme</button>
 
+  <button id="recoverBtn">¿Olvidaste tu contraseña?</button>`
+}
+// inicio de sesión
+document.getElementById('logIn').addEventListener('click', (prevent) => {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  prevent.preventDefault();
+  logIn(email,password);
+   
+})
+// función que realiza el incio de sesión en firebase
+function logIn(email, password) { 
+  firebase.auth().signInWithEmailAndPassword(email, password)
+   
+  .catch(function(error) {
+      // Handle Errors here.
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      alert('Debe Ingresar su correo electrónico y Contraseña')
+      // ...
+      console.log(errorCode);
+      console.log(errorMessage);s
+    });
+  
+}
+
+function showHome (user) {
+  if(user.emailVerified) {
+        
+    root.innerHTML = `
+
+    <h1>DP</h1>
+    <br>
+    <button id="homeMuro">HOME</button>
+    <button id="perfilUsuario">fotoUsuario</button>
+    <br>
+    <!-- Buscador -->
+    <input type="text" id="searchMuro" class="searchClass" placeholder="Buscador de DovePLayer"></input>
+
+    <!-- Cerrar sesión -->
+    <button id="btnSignOff">Cerrar Sesión</button>
+    `  
+    
+   document.getElementById('btnSignOff').addEventListener('click', signOff)
+   function signOff() {
+  
+    firebase.auth().signOut()
+    .then(function(){
+      document.location.href="/";
+      //console.log('saliendo....')
+    })
+    .catch(function(error){
+      console.log('error')
+    });
+  }
+   
+  }
+}
 // REGISTRAR USUARIO
 function registerMe() {
   const root = document.getElementById('root');
@@ -34,6 +102,7 @@ function register() {
     firebase.auth().createUserWithEmailAndPassword(registerEmail, registerPassword)
     .then(function(){
       check()
+      alert('La cuenta se ha creado exitosamente');
     })
     .catch(function(error) {
         // Handle Errors here.
@@ -46,56 +115,8 @@ function register() {
       });    
 };
 
-// LOGEARSE Y CERRAR SESIÓN
-function logIn() { /* VARINEA meti mano*/
-
-
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-   
-     
-    root.innerHTML = `
-
-    <h1>DP</h1>
-    <br>
-    <button id="homeMuro">HOME</button>
-    <button id="perfilUsuario">fotoUsuario</button>
-    <br>
-    <!-- Buscador -->
-    <input type="text" id="searchMuro" class="searchClass" placeholder="Buscador de DovePLayer"></input>
-
-    <!-- Cerrar sesión -->
-    <button id="btnSignOff">Cerrar Sesión</button>
-    `  
-    
-   document.getElementById('btnSignOff').addEventListener('click', signOff)
-    firebase.auth().signInWithEmailAndPassword(email, password)
-   
-    .catch(function(error) {
-        // Handle Errors here.
-        let errorCode = error.code;
-        let errorMessage = error.message;
-        alert('Debe Ingresar su correo electrónico y Contraseña')
-        // ...
-        console.log(errorCode);
-        console.log(errorMessage);s
-      });
-    
-
-}
-
 // funcion CERRAR SESIÓN
-function signOff() {
-  
-  firebase.auth().signOut()
-  .then(function(){
-    document.location.href="/";
-    //console.log('saliendo....')
-  })
-  .catch(function(error){
-    console.log('error')
-  });
-}
+
  
 function check() {
   let user = firebase.auth().currentUser;
@@ -114,10 +135,9 @@ user.sendEmailVerification().then(function() {
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in.
-      logIn();
+      showHome(user);
       let displayName = user.displayName;
       let email = user.email;
-      console.log(user.emailVerified)
       let emailVerified = user.emailVerified;
       let photoURL = user.photoURL;
       let isAnonymous = user.isAnonymous;
@@ -125,12 +145,10 @@ user.sendEmailVerification().then(function() {
       let providerData = user.providerData;
       // ...
     } else {
-      // User is signed out.
-      // ...
+    
     }
 
   });
 
  }
  observer();
-
