a95367178dfb1eaebc3b46ae0a09eca073d38b9b
Varinea <varinea.lopez@gmail.com>
2020-01-28 23:09:22 -0300

    Agregando la sección de post

--- src/lib/post.js
+++ src/lib/post.js
@@ -1,29 +1,59 @@
-// Initialize Cloud Firestore through Firebase
-
-function postUsuario() {
-  const root2 = document.getElementById('root2')
-  root2.innerHTML=`<header id="root2">
-  <button id="Gmail">Ingresa Aqui</button>
-</header>
-`
-}
-
+// INICIO DE POST -- VARINEA //
 firebase.initializeApp({
   apiKey: "AIzaSyCJK1y95OD8KAEsm8rzYjeZLcwyJ6Hfa5A",
   authDomain: "social-network-7c958.firebaseapp.com",
-  projectId: "social-network-7c958"
+  projectId: "social-network-7c958",
 });
 
 var db = firebase.firestore();
 
+//agregar documentos
+
+document.getElementById('postbutton').addEventListener('click',function(){
+let postTittle2 = document.getElementById('postTittle').value;
+let postText2 = document.getElementById('postText').value;	
+
 db.collection("users").add({
-  first: "Ada",
-  last: "Lovelace",
-  born: 1815
-})
-.then(function(docRef) {
-  console.log("Document written with ID: ", docRef.id);
-})
-.catch(function(error) {
-  console.error("Error adding document: ", error);
+      Titulo: postTittle2,
+      Texto: postText2,
+  })
+  .then(function (docRef) {
+      console.log("Document written with ID: ", docRef.id);
+      document.getElementById('postTittle').value = ''; // Una vez se haya generado el dato se dara un string limpio (reseteara la pag)
+      document.getElementById('postText').value = '';
+
+  })
+  .catch(function (error) {
+      console.error("Error adding document: ", error);
+        })
 });
+
+// LEER DOCUMENTOS
+
+db.collection("users").onSnapshot((querySnapshot) => {
+  postUsuario.innerHTML = '';
+
+  querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data().Titulo}`);
+      postUsuario.innerHTML += 
+      
+      ` 
+      <h2 id="tittle">${doc.data().Titulo} </h2> 
+      <textarea id="text">${doc.data().Texto}</textarea>
+      <button id="postDeleted" onclick="postDeleted('${doc.id}')"> Borrar </button>
+      <button id="postEditUs" onclick="postEditUs('${doc.id}','${doc.data().Titulo}','${doc.data().Texto}')"> Editar </button>
+      `
+  });
+});
+
+// ELIMINAR DOCUMENTOS
+
+function postDeleted(id) {
+  db.collection("users").doc(id).delete().then(function() {
+      console.log("Vaya, vaya, has eliminado el post correctamente!");
+  }).catch(function(error) {
+      console.error("Ups!, Ocurrio un error: ", error);
+  });
+
+};
+
