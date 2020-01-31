78b4017e23686ce348e943ed9c37e9dd5a7b5bb3
SusejR <rodriguezbsusej@gmail.com>
2020-01-31 09:37:43 -0300

    subiendo cambios

--- src/lib/loginGmail.js
+++ src/lib/loginGmail.js
@@ -1,19 +1,19 @@
-// document.getElementById('btngmail').addEventListener('click',authFire)
-// export const authFire = () => {
 
-//   const provider = new firebase.auth.GoogleAuthProvider();
-//   provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-//   firebase.auth().signInWithPopup(provider).then(function(result){
-//     let token= result.credencial.accessToken;
-//   let user =resul.user;
-// console.log(user);
-// }).catch(function(error) {
-// let errorCode= error.code;
-// let errorMessage= error.message;
-// let email= error.email;
-// let credential= error.credential;
-// console.error(error)
-// });
-// }
+export const authFire = () => {
 
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+  firebase.auth().signInWithPopup(provider).then(function(result){
+    let token= result.credencial.accessToken;
+  let user =resul.user;
+console.log(user);
+}).catch(function(error) {
+let errorCode= error.code;
+let errorMessage= error.message;
+let email= error.email;
+let credential= error.credential;
+console.error(error)
+});
+}
+document.getElementById('btngmail').addEventListener('click', authFire)
 
